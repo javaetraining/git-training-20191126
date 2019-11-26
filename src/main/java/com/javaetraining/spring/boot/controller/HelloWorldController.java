233f1d8c6a839e2268653ce22643b2df409d6d0b
Mahesh 2 <mahesh@javaetraining.com>
2019-11-26 22:48:29 +0530

    changes by user 2

--- src/main/java/com/javaetraining/spring/boot/controller/HelloWorldController.java
+++ src/main/java/com/javaetraining/spring/boot/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javaetraining.spring.boot.model.HelloWorldBean;
 
 // changes by user 123
+//Logic Dbcvdbv
 @RestController
 public class HelloWorldController {
 
