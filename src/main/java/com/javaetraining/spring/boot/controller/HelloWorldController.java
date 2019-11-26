8c86a9615ef6c5b77cc80425e8f59c5f467ba57f
Mahesh Gurrapu <mahesh@javaetraining.com>
2019-11-26 22:42:07 +0530

    chnages by user1

--- src/main/java/com/javaetraining/spring/boot/controller/HelloWorldController.java
+++ src/main/java/com/javaetraining/spring/boot/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.javaetraining.spring.boot.model.HelloWorldBean;
 
-// changes by user 1
+// changes by user 123
 @RestController
 public class HelloWorldController {
 
